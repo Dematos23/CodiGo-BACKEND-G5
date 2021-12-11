b2b2520265b44de7ebeb4532edb9d961f3831aec
Diego Matos <47648806+Dematos23@users.noreply.github.com>
2021-12-10 21:49:15 -0500

    chore: inicio de operadores

--- sesion1/01-variable.py
+++ sesion1/01-variable.py
@@ -29,7 +29,49 @@ edades = [10, 12, 40, 60, 'Eduardo', 14,5, False, [1,2]]
 # JSON
 curso = {
     'nombre': 'Backend',
-    'dificultad': 'Dificil'
+    'dificultad': 'Dificil',
+    'skills': [
+        {
+            'nombre':'Base de datos',
+            'nivel': 'Intermedio'
+        },
+        {
+            'nombre': 'ORM',
+            'nivel': 'Avanzado'
+        }
+    ]
 }
 
-print(curso['dificultad'])
+print(edades[2:-1])
+
+print(curso['skills'][1]['nombre'])
+
+personas = [{
+    'nombre': 'Eduardo',
+    'nacionalidad': 'peruano',
+    'hobbies':[
+        {
+            'nombre': 'Volar drones',
+            'experiencia': '2 años'
+        },{
+            'nombre':'Programar',
+            'experiencia': '1 mes'
+        }
+    ]
+},{
+    'nombre': 'Juliana',
+    'nacionalidad': 'colombiana',
+    'hobbies':[
+        {
+            'nombre': 'Montar bici',
+            'experiencia': '4 años'
+        },{
+            'nombre':'Bases de datos',
+            'experiencia': '8 mes'
+        }
+    ]
+}]
+
+print(personas[0]['nacionalidad'])
+print(personas[1]['hobbies'])
+print(personas[0]['hobbies'][1]['experiencia'])
