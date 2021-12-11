e55b2242747a9cf4bd6397597d35e99fd13f8378
Diego Matos <47648806+Dematos23@users.noreply.github.com>
2021-12-10 19:54:19 -0500

    primer break

---
+++ sesion1/01-variable.py
@@ -0,0 +1,35 @@
+# variable numerica
+numero =10
+numero2 = 10.5
+
+
+
+nombre ="Diego"
+apellido = 'Matos'
+
+html='''<html>
+<P>
+<P>
+'''
+
+fecha = 0
+
+print('holaaa :)')
+print(type(nombre))
+print(type(numero))
+
+soltero=True
+calor=False
+print(type(soltero))
+
+# Variables que tienen varios valores
+# arreglos 
+edades = [10, 12, 40, 60, 'Eduardo', 14,5, False, [1,2]]
+
+# JSON
+curso = {
+    'nombre': 'Backend',
+    'dificultad': 'Dificil'
+}
+
+print(curso['dificultad'])
