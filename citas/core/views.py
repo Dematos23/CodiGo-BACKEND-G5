c6522a9fe28b9136a11eaed6fc4942f7f2044a11
Diego Matos <47648806+Dematos23@users.noreply.github.com>
2022-01-16 06:22:11 -0500

    chore: limpieza

--- citas/core/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.generics import CreateAPIView
-from .serializers import PersonasSerializer
-from rest_framework.response import Response
-
-class PersonasController(CreateAPIView):
-    serializer_class=PersonasSerializer
-    def post(self, request):
-        print(request)
-        return Response(data='Hola amigos')
