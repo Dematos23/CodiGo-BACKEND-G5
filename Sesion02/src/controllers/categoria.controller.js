5dd562b9c293239290de50490913a4095f187e1f
Diego Matos <47648806+Dematos23@users.noreply.github.com>
2022-01-26 19:25:49 -0500

    terminado

--- Sesion02/src/controllers/categoria.controller.js
+++ Sesion02/src/controllers/categoria.controller.js
@@ -9,4 +9,40 @@ export const crearCategoria = async (req, res) => {
 export const listaCategoria = async(req, res) => {
     const categorias = await Prisma.categoria.findMany({})
     return res.json({content: categorias})
+};
+
+export const buscarCategoria = async (req, res) => {
+    console.log(req.query);
+    const params = req.query;
+
+    if (params.estado) {
+        params.estado = params.estado === "true" ? true : false;
+    }
+    
+    const categorias = await Prisma.categoria.findMany({ where: params});
+    
+    return res.json({
+        content: categorias,
+    })
 }
+
+export const actualizarCategoria = async(req, res) => {
+    const id = +req.params.id;
+    try {
+        await Prisma.categoria.findUnique({where: {id} });
+
+        const categoriaActualizada = await Prisma.categoria.update({
+            data: req.body, 
+            where: {id} 
+        });
+
+        return res.status(201).json({
+            content: categoriaActualizada,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({
+            message: "Error al actualizar la categoria",
+        });
+    }
+};
--- Sesion02/src/routes/categoria.routes.js
+++ Sesion02/src/routes/categoria.routes.js
@@ -1,6 +1,15 @@
 import {Router} from "express";
-import {crearCategoria, listaCategoria} from '../controllers/categoria.controller.js';
+import {
+    actualizarCategoria,
+    buscarCategoria,
+    crearCategoria, 
+    listaCategoria,
+} from '../controllers/categoria.controller.js';
 
 export const categoriaRouter = Router();
 
 categoriaRouter.route('/categoria').post(crearCategoria).get(listaCategoria);
+
+categoriaRouter.get("/buscarCategoria", buscarCategoria);
+
+categoriaRouter.route("/categoria/:id").put(actualizarCategoria)
